6789fdfc4c553747ff97484fb71d4ad57e5dd448
Anthonchess <anthonchess@gmail.com>
2025-06-09 13:33:20 -0400

    funcion para filtrar producto

--- papotin.js
+++ papotin.js
@@ -1,3 +1,9 @@
-const productos = [mangos, platano, yautia, arroz]
+const productos = ["mangos", "platano", "yautia", "arroz"]
 
 console.log(productos)
+
+const productos2 = productos.filter(function(palo){
+    if(palo === "mangos"){
+        console.log("pera")
+    }
+})
